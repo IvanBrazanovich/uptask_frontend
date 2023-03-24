cb83b2dd5e7ef6ed4b34a0154f6ee53bb36bcd9f
Ivan <ivanbrazas@gmail.com>
2023-03-23 22:57:54 -0300

    style commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,9 +17,11 @@ const Header = () => {
 
   return (
     <header className="bg-white p-3 flex justify-between items-center shadow-sm">
-      <h1 className="text-3xl font-black text-sky-600 tracking-tighter">
-        UpTask
-      </h1>
+      <Link to="/proyectos" className="font-bold  tracking-tight uppercase">
+        <h1 className="text-3xl font-black text-sky-600 tracking-tighter">
+          UpTask
+        </h1>
+      </Link>
 
       <input
         type="text"
